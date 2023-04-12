a9479c6756b45d4b3db84ec0d5fd2d99857649b7
Iyan Theekshana <iyan.dev123@gmail.com>
2023-04-12 10:29:00 +0200

    Encap completed

--- LezioneUno/LezioneUno/Encap3.cs
+++ LezioneUno/LezioneUno/Encap3.cs
@@ -67,18 +67,70 @@ namespace LezioneUno
             }
         }
 
-        class Test3
-        {
-            static void Main(string[] args)
-            {
-                Book b = new Book();
-                b.SetNumP(20);
-                Console.WriteLine(b.GetNumP());
+    }
 
-                Employee e = new Employee();
-                e.Name = "Iyan";
-                Console.WriteLine(e.Name);
-            }
+    class SavingAccount
+    {
+
+        public string Oname { get; set; } // Automatic property
+
+        // read-only automatic property
+        public int MyV { get; }
+
+        // write-only automatic property - ERROR on AUTOMATIC PROPERTY
+        //public int MyV2 { set; }
+
+        //..
+        private static double _currentInterestRate = 0.04;
+
+        //static property
+        public static double InterstRate
+        {
+            get { return _currentInterestRate; }
+            set { _currentInterestRate = value; }
         }
     }
+
+
+    class Garage
+    {
+        // Assegnare il valore per Automatic property
+        public int NumOfCars { get; set; } = 1;
+
+        // has-a
+        public Car MyAuto { get; set; } = new Car();
+
+
+        public Garage()
+        {
+
+        }
+        public Garage(Car c, int number)
+        {
+            MyAuto = c;
+            NumOfCars = number;
+        }
+
+
+    }
+
+    class Test3
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            b.SetNumP(20);
+            Console.WriteLine(b.GetNumP());
+
+            Employee e = new Employee();
+            e.Name = "Iyan";
+            Console.WriteLine(e.Name);
+
+            SavingAccount.InterstRate = 2;
+            SavingAccount s1 = new SavingAccount();
+            s1.Oname = "aaa";
+            Console.WriteLine(s1.Oname);
+        }
+    }
+
 }
